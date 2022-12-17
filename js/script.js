6481e4b69b71819d9820886c133e7b84cfcceb5b
unknown <dheepi117@gmail.com>
2022-12-17 12:41:50 +0530

    add problem statement.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var age=parseInt(prompt("enter your age"));
+if(age>18)
+{
+	if(true)
+	{
+		alert("Eligible for vote");
+	}
+	
+	else
+	{
+		alert("Not Eligible");
+	}
+}
+else
+{
+	alert("Age is Not eligible for vote");
+}
+
